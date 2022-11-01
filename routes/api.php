fe4cdee50df42c5f9aec79e1515a223b10febffd
dhodo999 <dashdho2@gmail.com>
2022-11-01 09:16:43 +0700

    migration + sanctum

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,11 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\SiswaController;
+use App\Http\Controllers\BookController;
+use App\Http\Controllers\AuthController;
+
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -14,6 +19,47 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+// Route::get('/halo', function(){
+//     return ["dhodo" => "perngawian"];
+// });
+
+// Route::get('HaloController', [HaloController::class, 'index']);
+// Route::resource('/', Api\HaloController::class);
+
+Route::resource('siswa', SiswaController::class);
+
+// Route::get('/index', [SiswaController::class, 'index'])->name('index');
+//     Route::get('/create', [SiswaController::class, 'create'])->name('create');
+//     Route::post('/store', [SiswaController::class, 'store'])->name('store');
+//     Route::get('/show/{id}', [SiswaController::class, 'show'])->name('show');
+//     Route::post('/update/{id}', [SiswaController::class, 'update'])->name('update');
+//     Route::get('/destroy/{id}', [SiswaController::class, 'destroy'])->name('destroy');
+
+// Route::resource('book', BookController::class);
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request){
     return $request->user();
 });
+
+// public route
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::get('/books', [BookController::class, 'index']);
+Route::get('/books/{id}', [BookController::class, 'show']);
+// Route::get('/authors', [AuthorController::class, 'index']);
+// Route::get('/authors/{id}', [AuthorController::class, 'index']);
+
+// protected routes
+Route::middleware('auth:sanctum')->group(function(){
+    Route::resource('books', BookController::class)->except('create', 'edit', 'show', 'index');
+    Route::post('/logout', [AuthController::class, 'logout']);
+    // Route::resource('authors', AuthorController::class)->except('create', 'edit', 'show', 'index');    
+});
+
+
+
